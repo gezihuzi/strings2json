b97c702ca20329b2556a8cf93791ba5fb75198b7
gezihuzi <a495008890@live.com>
2023-11-07 20:11:13 +0800

    feat: 完善单个文件的转换和获取

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,55 @@
+use clap::Parser;
+use regex::{Captures, Regex};
 use serde_json::{json, Value};
-use std::fs::{self, File};
+use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
+use std::path::{Path, PathBuf};
 
-fn main() {
-    // 获取当前目录
-    let mut current_dir = std::env::current_dir().expect("Failed to get current directory");
-    current_dir.push("strings");
-    // 遍历当前目录下的所有文件和目录
-    for entry in fs::read_dir(&current_dir).expect("Failed to read directory") {
-        if let Ok(entry) = entry {
-            let path = entry.path();
-            // 检查是否是.strings文件
-            if let Some(extension) = path.extension() {
-                if extension == "strings" {
-                    // 执行转换操作
-                    convert_to_json(&path);
-                }
-            }
-        }
-    }
-
-    println!("Conversion complete!");
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    input: PathBuf,
+    #[arg(short, long)]
+    output: PathBuf,
 }
 
-fn convert_to_json(file_path: &std::path::Path) {
-    println!("Converting {:?}", file_path);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    // 获取当前目录
+    let path = args.input;
+    // 执行转换操作
+    let json = strings_to_json(&path)?;
+    // 输出到文件
+    let path = &args.output;
+    let mut output = File::create(path)?;
+    write!(output, "{}", json)?;
+    println!("Conversion complete!");
+    Ok(())
+}
+
+fn strings_to_json(path: &Path) -> Result<String, Box<dyn std::error::Error>> {
+    println!("Converting {:?}", path);
 
     // 打开.strings文件
-    let file = File::open(file_path).expect("Failed to open input file");
+    let file = File::open(path).expect("Failed to open input file");
     let reader = BufReader::new(file);
 
     // 创建一个空的JSON对象
-    let mut json_obj = json!({});
+    let mut json = json!({});
 
     for line in reader.lines() {
         if let Ok(line_str) = line {
             // 解析每一行，提取出key和value
             if let Some((key, value)) = parse_line(&line_str) {
                 // 添加到JSON对象中
-                json_obj[key] = Value::String(value);
+                json[key] = Value::String(value);
             }
         }
     }
 
-    // 将JSON对象写入输出文件
-    let output_file_name = file_path.with_extension("json");
-    let mut output = File::create(output_file_name).expect("Failed to create output file");
-    let json_data = serde_json::to_string_pretty(&json_obj).expect("Failed to serialize JSON");
-    write!(output, "{}", json_data).expect("Failed to write JSON to file");
+    let json = serde_json::to_string_pretty(&json)?;
+    Ok(json)
 }
 
 fn parse_line(line: &str) -> Option<(String, String)> {
@@ -55,7 +57,14 @@ fn parse_line(line: &str) -> Option<(String, String)> {
         let parts: Vec<&str> = line.splitn(2, " = ").collect();
         if parts.len() == 2 {
             let key = parts[0].trim_end_matches(';').trim_matches('"').to_string();
-            let value = parts[1].trim_matches('"').to_string();
+            let mut value = parts[1].trim_end_matches(';').trim_matches('"').to_string();
+
+            // 将%1$@，%2$@等转换为${t1}，${t2}等
+            let re = Regex::new(r"%(\d+)\$@").unwrap();
+            value = re
+                .replace_all(&value, |caps: &Captures| format!("${{t{}}}", &caps[1]))
+                .to_string();
+
             return Some((key, value));
         }
     }
